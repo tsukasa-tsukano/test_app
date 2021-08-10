a91c6108266f5245f5ed7325247c9c61577602b3
tsukasa-tsukano <79680403+tsukasa-tsukano@users.noreply.github.com>
2021-08-10 16:41:30 +0900

    Delete practice directory

--- practice/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var today = new Date();
-    var y = today.getFullYear();
-    $('#calendar').multiDatesPicker({
-        numberOfMonths: [4,3],
-        defaultDate: '1/1/'+y
-    });
-});
